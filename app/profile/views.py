ad2a4bca7f592cb4992a2a65eae8451f7161369f
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-08-29 15:45:33 -0400

    password update with success and error messages

--- app/profile/views.py
+++ app/profile/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
-from app.forms import ProfileUpdateForm
+from django.urls import reverse
+from django.contrib import messages
+from app.forms import ProfileUpdateForm, ChangePasswordForm
 
 
 @login_required
@@ -15,4 +16,20 @@ def profile_view(request):
             print(form.errors)
     else:
         form = ProfileUpdateForm(instance=request.user)
-    return render(request, 'profile/profile.html', {'form': form})
+        password_form = ChangePasswordForm(request.user)
+    return render(request, 'profile/profile.html', {'form': form, 'password_form': password_form})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        password_form = ChangePasswordForm(request.user, request.POST)
+        if password_form.is_valid():
+            password_form.save()
+            messages.success(request, '¡Contraseña cambiada exitosamente!')
+            return redirect('login')
+        else:
+            for field, errors in password_form.errors.items():
+                for error in errors:
+                    messages.error(request, error)
+            return redirect('profile')
+    return redirect('profile')
