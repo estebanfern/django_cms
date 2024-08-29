d3dff8bcb9a58c72399589e92361854c4fa3cf2d
Roberto Daniel Acosta Yudis <rdayudis@gmail.com>
2024-08-29 18:13:18 -0400

    User manager changes

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,7 @@ from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.models import Group
 from django.contrib import admin, messages
 from app.models import CustomUser
+from django import forms
 
 @admin.action(description="Bloquear usuario/s")
 def bloquear_usuarios(self, request, queryset):
@@ -19,10 +20,30 @@ def desbloquear_usuarios(self, request, queryset):
             usuario.save()
             self.message_user(request, f'El usuario {usuario.name} ha sido desbloqueado.', messages.SUCCESS)
 
+
+# class CustomUserForm(forms):
+#     class Meta:
+#         model = CustomUser
+#         fields = ['name', 'email', 'is_active','user_permissions', 'groups']
+#         labels = {
+#             'is_active': 'Activo',  # Cambiar el label aquí
+#         }
+
+
 @admin.register(CustomUser)
 class UserAdmin(admin.ModelAdmin):
+    # form = CustomUserForm
     list_display = ('name', 'email', 'is_active')
+    readonly_fields = ('name', 'email')
     actions = [bloquear_usuarios, desbloquear_usuarios]
+    exclude = ('password',)
+
+
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'email', 'is_active','user_permissions', 'groups')
+        }),
+    )
 
     def has_module_permission(self, request):
         return request.user.is_staff and request.user.has_perm('app.view_users')
