50b602d6f76e89af6008e3202b1ce15820afd960
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-08-30 20:28:19 -0400

    password reset logic and email service

--- cms/settings.py
+++ cms/settings.py
@@ -160,3 +160,11 @@ DEFAULT_FILE_STORAGE = 'cms.storage_backends.PublicMediaStorage'
 DEFAULT_PROFILE_PHOTO=config('DEFAULT_PROFILE_PHOTO')
 
 LOGIN_URL = '/login'
+
+#EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
