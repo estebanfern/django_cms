c04f3bc076cd9a1a488fea488a1173bfbc594445
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-03 17:29:14 -0400

    fix date_published render

--- content/forms.py
+++ content/forms.py
@@ -6,12 +6,10 @@ class ContentForm(forms.ModelForm):
         model = Content
         fields = ['title', 'summary', 'category', 'date_published', 'content', 'attachment']
         widgets = {
-            'summary': forms.Textarea(attrs={'style': 'display: block; width: 100%;'}),  # Asegura que ocupe toda la línea
-            #'date_published': forms.DateInput(attrs={'type': 'date', 'style': 'display: block; width: 100%;'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),  # Campo de texto con clase Bootstrap
             'summary': forms.Textarea(attrs={'class': 'form-control', 'rows': '4', 'maxlength' : '255'}),  # Asegura que ocupe toda la línea
             'category': forms.Select(attrs={'class': 'form-select'}),  # Campo select con clase Bootstrap
-            'date_published': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),  # Campo de fecha con clase Bootstrap
+            'date_published': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'attachment': forms.ClearableFileInput(attrs={'class': 'form-control'}),  # Campo de archivo con clase Bootstrap
         }
     def __init__(self, *args, **kwargs):
@@ -22,6 +20,7 @@ class ContentForm(forms.ModelForm):
                 # En modo de edición
                 self.fields['category'].required = False
                 self.fields['category'].widget.attrs['disabled'] = 'disabled'
+                self.fields['date_published'].widget = forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'readonly': 'readonly'})
         else:
             # En modo de creación
-            self.fields['date_published'].widget = forms.DateInput(attrs={'type': 'date'})
+            self.fields['date_published'].widget = forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
