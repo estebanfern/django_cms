4327e041f70366ccdec6dfd06e7b0b5bec59f378
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-05 21:46:16 -0400

    add query search

--- app/views.py
+++ app/views.py
@@ -13,14 +13,13 @@ from .models import CustomUser
 def home_view(request):
     cat_query = request.GET.get('cat')
     category = None
-    autor_query = request.GET.get('autor')
-    autor = None
+    query = request.GET.get('query')
     contents = Content.objects.filter(is_active=True).select_related('category', 'autor').order_by('-date_create')
     if cat_query:
         category = Category.objects.get(id=cat_query)
         contents = contents.filter(category_id=cat_query)
-    if autor_query:
-        autor = CustomUser.objects.get(id=autor_query)
+    if query:
+        contents = contents.filter(title__icontains=query)
     paginator = Paginator(contents, 10)
     str_page_number = request.GET.get('page')
     if str_page_number is None or str_page_number == '':
@@ -29,4 +28,4 @@ def home_view(request):
         page_number = int(str_page_number)
     if (page_number is None) or (page_number < 1): page_number = 1
     page_obj = paginator.get_page(page_number)
-    return render(request, 'inicio.html', {'page_obj': page_obj, 'category': category, 'autor': autor})
+    return render(request, 'inicio.html', {'page_obj': page_obj, 'category': category, 'query': query})
