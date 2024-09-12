c6d9e6be2b1abf05a352644a43b403dc85528f6c
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-11 22:54:41 -0400

    CMS-204, CMS-206 | add dinamic filter and redirection for unauthenticated users

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from category.models import Category
 from content.models import Content
-
+from django.contrib import messages
 
 
 # Create your views here.
@@ -13,13 +13,28 @@ def home_view(request):
     category = None
     query = request.GET.get('query')
     contents = Content.objects.filter(is_active=True, state=Content.StateChoices.publish).select_related('category', 'autor').order_by('-date_create')
+
+    # Si se busco una categoria, verificar si el usuario está logueado en caso de que no sea categoria publica
+    # si no está loguead, redirigirle al login con un mensaje
+    # TODO: suscripciones a categorias de pago
     if cat_query:
-        category = Category.objects.get(id=cat_query)
+        category = get_object_or_404(Category, id=cat_query)
+        if not request.user.is_authenticated and not category.type == Category.TypeChoices.public:
+            messages.warning(request, 'Para poder acceder a categorias de suscripción o pago debes estar registrado')
+            return redirect('login')
         contents = contents.filter(category_id=cat_query)
+    # Si no se filtra por categoria, verificar si el usuario está logueado, y si no mostrar solo los contenidos de categorias públicas
+    # TODO: para usuarios logueados, mostrar los contenidos de categorias de pago a los que están suscriptos
+    else:
+        if not request.user.is_authenticated:
+            contents = contents.filter(category__type=Category.TypeChoices.public)
+
+    # Si ingreso un parámetro de buscar, filtrar con un OR por titulo y nombre del autor
     if query:
         contents = contents.filter(
             Q(title__icontains=query) | Q(autor__name__icontains=query)
         )
+
     paginator = Paginator(contents, 10)
     str_page_number = request.GET.get('page')
     if str_page_number is None or str_page_number == '':
