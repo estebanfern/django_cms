ff1e6a80cf65ea9931c48651d5d5500f89186baf
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-13 00:16:14 -0400

    add env variables for migrate and colelctstatic on dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-python manage.py migrate
-python manage.py collectstatic --no-input
+
+# Ejecutar las migraciones solo si se especifica
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+    python manage.py migrate
+fi
+
+# Ejecutar collectstatic solo si se especifica
+if [ "$RUN_COLLECTSTATIC" = "true" ]; then
+    python manage.py collectstatic --no-input
+fi
+
 gunicorn cms.wsgi:application -c gunicorn.conf.py
