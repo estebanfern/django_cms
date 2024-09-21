c8c02f695090d0a0338e5adf133e5a85efd2c535
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-20 22:49:53 -0400

    fix rating null on create

--- rating/models.py
+++ rating/models.py
@@ -10,7 +10,7 @@ from content.models import Content
 class Rating(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    rating = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
