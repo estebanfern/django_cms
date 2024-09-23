52047cb27630229a6f22e3898411f53cb47ab715
Esteban Fernandez <estebangfernandeza@gmail.com>
2024-09-22 23:09:44 -0400

    add log info on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,13 @@
 if [ "$1" = "workers" ]; then
     # shift  # Remover "celery" del argumento
     # celery "$@"  # Ejecutar el comando de celery con los argumentos restantes
+    echo "Executing workers"
     celery -A cms worker -f /app/logs/workers.log
 elif [ "$1" = "scheduled" ]; then
+    echo "Executing scheduled tasks"
     celery -A cms beat -f /app/logs/scheduled.log
 else
+    echo "Executing app"
     # Ejecutar las migraciones solo si se especifica
     if [ "$RUN_MIGRATIONS" = "true" ]; then
         python manage.py migrate
