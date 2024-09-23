9de1d2dc3a3ea5de6926b946e541814c21a6e882
fabri10roman <fabri11roman.fr@gmail.com>
2024-09-22 22:39:52 -0400

    add redis cache

--- cms/profile/prod.py
+++ cms/profile/prod.py
@@ -58,3 +58,13 @@ LOGGING = {
     },
 }
 
+# REDIS CACHE
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{config('REDIS_HOST', default='localhost:6379')}/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
