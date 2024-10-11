3f34e4294650538ff8da60896a295234b131dc4e
Abigail Fernandez <mercedesfernandes319@gmail.com>
2024-10-11 12:05:44 -0300

    CMS-302: Statistics documentation📄

--- stadistic/views.py
+++ stadistic/views.py
@@ -10,6 +10,25 @@ def view_stadistics(request):
 
 @login_required
 def top_liked(request):
+    """
+    Devuelve los 10 contenidos con más "me gusta" del autor autenticado en formato JSON.
+
+    Requiere:
+        El usuario debe estar autenticado.
+
+    Parámetros:
+        request (HttpRequest): Objeto de solicitud HTTP realizado por el usuario autenticado.
+
+    Comportamiento:
+        - Filtra los contenidos publicados creados por el usuario autenticado.
+        - Obtiene los 10 contenidos con más "me gusta", incluyendo los campos: título, número de "me gusta", fecha de creación y fecha de publicación.
+        - Ordena los resultados por el número de "me gusta" en orden descendente.
+        - Retorna los datos en formato JSON con el estado de éxito.
+
+    Retorna:
+        JsonResponse: Respuesta con los datos de los contenidos más valorados en formato JSON.
+    """
+
     user = request.user
     top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now())\
                                     .values('title', 'likes_count', 'date_create', 'date_published')\
@@ -25,6 +44,25 @@ def top_liked(request):
 
 @login_required
 def top_disliked(request):
+    """
+    Devuelve los 10 contenidos con más "no me gusta" del autor autenticado en formato JSON.
+
+    Requiere:
+        El usuario debe estar autenticado.
+
+    Parámetros:
+        request (HttpRequest): Objeto de solicitud HTTP realizado por el usuario autenticado.
+
+    Comportamiento:
+        - Filtra los contenidos publicados creados por el usuario autenticado.
+        - Obtiene los 10 contenidos con más "no me gusta", incluyendo los campos: título, número de "no me gusta", fecha de creación y fecha de publicación.
+        - Ordena los resultados por el número de "no me gusta" en orden descendente.
+        - Retorna los datos en formato JSON con el estado de éxito.
+
+    Retorna:
+        JsonResponse: Respuesta con los datos de los contenidos más rechazados en formato JSON.
+    """
+
     user = request.user
     top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now())\
                                     .values('title', 'dislikes_count', 'date_create', 'date_published')\
@@ -40,6 +78,25 @@ def top_disliked(request):
 
 @login_required
 def top_rating(request):
+    """
+    Devuelve los 10 contenidos mejor valorados del autor autenticado en formato JSON.
+
+    Requiere:
+        El usuario debe estar autenticado.
+
+    Parámetros:
+        request (HttpRequest): Objeto de solicitud HTTP realizado por el usuario autenticado.
+
+    Comportamiento:
+        - Filtra los contenidos publicados creados por el usuario autenticado.
+        - Obtiene los 10 contenidos con el promedio de calificaciones más alto, incluyendo los campos: título, promedio de calificaciones, fecha de creación y fecha de publicación.
+        - Ordena los resultados por el promedio de calificaciones en orden descendente.
+        - Retorna los datos en formato JSON con el estado de éxito.
+
+    Retorna:
+        JsonResponse: Respuesta con los datos de los contenidos mejor valorados en formato JSON.
+    """
+
     user = request.user
     top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now()) \
                                     .values('title', 'rating_avg', 'date_create', 'date_published') \
@@ -55,6 +112,25 @@ def top_rating(request):
 
 @login_required
 def top_view(request):
+    """
+    Devuelve los 10 contenidos más vistos del autor autenticado en formato JSON.
+
+    Requiere:
+        El usuario debe estar autenticado.
+
+    Parámetros:
+        request (HttpRequest): Objeto de solicitud HTTP realizado por el usuario autenticado.
+
+    Comportamiento:
+        - Filtra los contenidos publicados creados por el usuario autenticado.
+        - Obtiene los 10 contenidos con más vistas, incluyendo los campos: título, número de vistas, fecha de creación y fecha de publicación.
+        - Ordena los resultados por el número de vistas en orden descendente.
+        - Retorna los datos en formato JSON con el estado de éxito.
+
+    Retorna:
+        JsonResponse: Respuesta con los datos de los contenidos más vistos en formato JSON.
+    """
+
     user = request.user
     top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now()) \
                                     .values('title', 'views_count', 'date_create', 'date_published') \
