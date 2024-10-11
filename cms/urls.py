ce89885368eb2d06b63bf216a64deb2f818fe80c
Abigail Fernandez <mercedesfernandes319@gmail.com>
2024-10-11 11:59:06 -0300

    CMS-51 CMS-300: statistics of views and dislikes📊

--- cms/urls.py
+++ cms/urls.py
@@ -36,7 +36,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from category.views import categories_by_type
 from rating import views as rating_views
-from stadistic.views import view_stadistics, top_liked, top_rating
+from stadistic.views import view_stadistics, top_liked, top_rating, top_disliked, top_view
 from suscription.views import suscribe_category, unsuscribe_category
 
 
@@ -88,7 +88,9 @@ urlpatterns = [
     #Stadistics
     path('stadistics/', view_stadistics, name='view_stadistics'),
     path('stadistics/likes/', top_liked, name='top_liked'),
+    path('stadistics/dislikes/', top_disliked, name='top_disliked'),
     path('stadistics/rating/', top_rating, name='top_rating'),
+    path('stadistics/views/', top_view, name='top_view'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stadistic/views.py
+++ stadistic/views.py
@@ -24,11 +24,41 @@ def top_liked(request):
             )
 
 @login_required
+def top_disliked(request):
+    user = request.user
+    top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now())\
+                                    .values('title', 'dislikes_count', 'date_create', 'date_published')\
+                                    .order_by('-dislikes_count')[:10]
+    data =  {
+        'status': 'success',
+        'result': list(top_contents)
+    }
+    return JsonResponse(
+                data,
+                safe=False
+            )
+
+@login_required
 def top_rating(request):
     user = request.user
     top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now()) \
                                     .values('title', 'rating_avg', 'date_create', 'date_published') \
                                     .order_by('-rating_avg')[:10]
+    data = {
+        'status': 'success',
+        'result': list(top_contents)
+    }
+    return JsonResponse(
+        data,
+        safe=False
+    )
+
+@login_required
+def top_view(request):
+    user = request.user
+    top_contents = Content.objects.filter(autor=user, state=Content.StateChoices.publish, date_published__lt=timezone.now()) \
+                                    .values('title', 'views_count', 'date_create', 'date_published') \
+                                    .order_by('-views_count')[:10]
     data = {
         'status': 'success',
         'result': list(top_contents)
