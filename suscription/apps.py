9db3e641b3ccf4e3717cb456e0575fce53f64077
fabri10roman <fabri11roman.fr@gmail.com>
2024-10-20 16:06:38 -0300

    delete unnecessary signal

--- suscription/apps.py
+++ suscription/apps.py
@@ -14,6 +14,3 @@ class SuscriptionConfig(AppConfig):
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'suscription'
-
-    def ready(self):
-        import suscription.signals
